97daf6bf6550937ada18198778bbeb8fe30e16f4
Aaron Morais <aaron.m@live.ca>
2016-02-28 16:28:14 -0500

    Move code from fcn_bindings to feature function

--- src/content_script/duplicate_program_detection.js
+++ src/content_script/duplicate_program_detection.js
@@ -17,6 +17,35 @@ glp.duplicateProgramDetection.disable = function() {
   this.duplicates.length = 0;
 }
 
+glp.duplicateProgramDetection.useProgramCalled = function(gl, program) {
+  var currentProgram = gl.getParameter(gl.CURRENT_PROGRAM);
+  if( currentProgram != undefined &&
+      currentProgram.__uuid != undefined &&
+      currentProgram.__uuid == program.__uuid ) {
+    /*
+     * callStack gets the current call stack information up to this point
+     */
+    var callStack = glpGetStack();
+    var userStack = glpGetFirstUserStack(callStack);
+    var lineNumber = ""
+    var functionName = "";
+    if (userStack != null) {
+      lineNumber = userStack.getLineNumber();
+      // Sometimes the function name can be undefined if
+      // it's called from a global scope or from an object
+      if (userStack.getFunctionName() != undefined) {
+        functionName = userStack.getFunctionName()
+      }
+
+      fileName = userStack.getFileName();
+    }
+    this.duplicates.push({"programId" : program.__uuid,
+                          "lineNumber" : lineNumber,
+                          "functionName" : functionName,
+                          "fileName" : fileName})
+  }
+}
+
 /**
  * Gets duplicate programs list from the time that enable is called
  * Sends duplicated program list to the front end
