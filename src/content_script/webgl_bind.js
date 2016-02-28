d75ce076bf1300ef75ad2e2520b084a9570f2401
Aaron Morais <aaron.m@live.ca>
2016-02-28 17:53:58 -0500

    Clean up binding

--- src/content_script/webgl_bind.js
+++ src/content_script/webgl_bind.js
@@ -11,29 +11,33 @@ function _glpBind(origFunc, newFunc, name) {
     }
 }
 
-WebGLRenderingContext.prototype.glp = glp;
+function bindWebGL() {
+    WebGLRenderingContext.prototype.glp = glp;
 
-/**
- * Bind WebGLRenderingContext functions to functions found in glpFcnBindings
- * If defined, functions are first bound the function found in glpFcnBindings
- * Afterwards, they are then bound to the default func
- */
-for (var name in WebGLRenderingContext.prototype) {
-    try {
-        if (typeof WebGLRenderingContext.prototype[name] != 'function') {
-            continue;
-        }
+    /**
+     * Bind WebGLRenderingContext functions to functions found in glpFcnBindings
+     * If defined, functions are first bound the function found in glpFcnBindings
+     * Afterwards, they are then bound to the default func
+     */
+    for (var name in WebGLRenderingContext.prototype) {
+        try {
+            if (typeof WebGLRenderingContext.prototype[name] != 'function') {
+                continue;
+            }
 
-        if (glpFcnBindings[name] != null) {
-            var newFunc = glpFcnBindings[name];
+            if (glpFcnBindings[name] != null) {
+                var newFunc = glpFcnBindings[name];
+                WebGLRenderingContext.prototype[name] =
+                    _glpBind(WebGLRenderingContext.prototype[name], newFunc, name);
+            }
+
+            var defaultFunc = glpFcnBindings["default"];
             WebGLRenderingContext.prototype[name] =
-                _glpBind(WebGLRenderingContext.prototype[name], newFunc, name);
+                _glpBind(WebGLRenderingContext.prototype[name], defaultFunc, name);
+        } catch(err) {
+            // TODO: Handle binding errors
         }
-
-        var defaultFunc = glpFcnBindings["default"];
-        WebGLRenderingContext.prototype[name] =
-            _glpBind(WebGLRenderingContext.prototype[name], defaultFunc, name);
-    } catch(err) {
-        // TODO: Handle binding errors
     }
 }
+
+bindWebGL();
