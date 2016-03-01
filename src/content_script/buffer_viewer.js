d4c875961cd77ba83e65d57a1c55580fa7a75b52
Victor Lai <lai.victor.vl@gmail.com>
2016-03-01 01:21:55 -0500

    Track buffer create calls

---
+++ src/content_script/buffer_viewer.js
@@ -0,0 +1,26 @@
+glp.bufferViewer = {};
+
+glp.bufferViewer.buffers = [];
+glp.bufferViewer.frameBuffers = [];
+glp.bufferViewer.renderBuffers = [];
+
+/**
+ * Sends the number of buffers created to the front end
+ **/
+glp.bufferViewer.getBuffers = function(gl) {
+
+}
+
+/**
+ * Sends the number of frame buffers created to the front end
+ **/
+glp.bufferViewer.getFrameBuffers = function(gl) {
+
+}
+
+/**
+ * Sends the number of render buffers created to the front end
+ **/
+glp.bufferViewer.getRenderBuffers = function(gl) {
+
+}
