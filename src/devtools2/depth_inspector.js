29c44f0aee56085ded3a90dcbbb633dbb5e9ae78
U-ADRIAN-PC\Adrian <a.m.cheung@hotmail.com>
2016-03-12 17:00:42 -0500

    Front end for depth inspector

--- src/devtools2/depth_inspector.js
+++ src/devtools2/depth_inspector.js
@@ -1,8 +1,14 @@
 define(["messages"], function (Messages) {
     var DepthInspector = React.createClass({
         toggleDepthInspector: function() {
-            var data = {"enabled": this.refs.depthInspector.checked};
-            Messages.sendMessage(this.props.activeContext, messageType.PIXEL_INSPECTOR, data);
+            var data = {
+                "enabled": this.refs.depthInspector.checked,
+                "range": {
+                    "near": this.refs.depthInspectorNear.value,
+                    "far": this.refs.depthInspectorFar.value
+                }
+            };
+            Messages.sendMessage(this.props.activeContext, messageType.DEPTH_INSPECTOR, data);
         },
         render: function() {
             return <div className="container">
@@ -10,11 +16,19 @@ define(["messages"], function (Messages) {
                     Depth Inspector
                 </div>
                 <div>
-                    Detects how how many times a pixel has been drawn. The color ranges from green to red, red being drawn multiple times while green been drawn only on clear.
+                    Displays the relative depths of the pixels being drawn.  Lighter values are further away whereas darker values are closer.
                 </div>
                 <div>
                     Enable Depth Inspector&nbsp;&nbsp;
                     <input ref="depthInspector" type="checkbox" onClick={this.toggleDepthInspector} />
+                </div>
+                <div>
+                    Near:&nbsp;<input ref="depthInspectorNear" type="number" defaultValue="1" onChange={this.toggleDepthInspector} />
+                    &nbsp;&nbsp;
+                    Far:&nbsp;<input ref="depthInspectorFar" type="number" defaultValue="1000" onChange={this.toggleDepthInspector} />
+                </div>
+                <div>
+                    Hint: If only white is displayed then the <b>far</b> value may be too small.  Likewise, if only black is displayed then the <b>far</b> value may be too large.
                 </div>
             </div>;
         }
